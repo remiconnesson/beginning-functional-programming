d94013f702f51730acd603ebd5235751dbdb9185
remiconnesson <remiconnesson2@gmail.com>
2022-08-11 10:09:27 +0000

    My solution with filter, reduce and dict accumulator

--- 01-python-intro/03_refactoring_a_loop.py
+++ 01-python-intro/03_refactoring_a_loop.py
@@ -14,3 +14,22 @@ for person in people:
 if height_count > 0:
     average_height = height_total / height_count
     print(average_height)
+
+# Functional Code
+from functools import reduce
+
+accumulateur = {"total":0, "count":0}
+
+def sum_and_count(acc, value):
+    acc["total"] += value
+    acc["count"] += 1
+    return acc
+
+people_with_heights = filter(lambda x: "height" in x, people)
+accumulateur = reduce(lambda a,x : sum_and_count(a, x["height"]),
+                      people_with_heights,
+                      accumulateur)
+
+if accumulateur["count"] > 0:
+    average_height = accumulateur["total"] / accumulateur["count"]
+    print(average_height)
