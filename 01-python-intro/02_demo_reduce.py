daff3cc10bfc0a0e45686625aacba0d8b5e2a9b1
remiconnesson <remiconnesson2@gmail.com>
2022-08-11 09:44:58 +0000

    reduce: count occurences of words in sentences

--- 01-python-intro/02_demo_reduce.py
+++ 01-python-intro/02_demo_reduce.py
@@ -16,3 +16,27 @@ sum_from_0_to_4 = reduce(lambda a, x : a + x,
                          list(range(5)))
 
 print(sum_from_0_to_4)
+
+"""
+Count Occurences of a word in a Sentences
+"""
+# Not functional
+sentences = ["Mary read a story to Sam and Isla.",
+             "Isla cuddled Same.",
+             "Sam chortled."]
+sam_count = 0
+for sentence in sentences:
+    sam_count += sentence.count("Sam")
+
+print(sam_count)
+
+# Functional
+sentences = ["Mary read a story to Sam and Isla.",
+             "Isla cuddled Same.",
+             "Sam chortled."]
+
+sam_count = reduce(lambda a, x: a + x.count("Sam"),
+                   sentences,
+                   0)
+
+print(sam_count)
