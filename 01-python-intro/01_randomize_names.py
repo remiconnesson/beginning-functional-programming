7648745e51470dac26b2cfd0af056562ba6ff9d2
remiconnesson <remiconnesson2@gmail.com>
2022-08-11 09:25:16 +0000

    hash each element of a list: for vs. map

--- 01-python-intro/01_randomize_names.py
+++ 01-python-intro/01_randomize_names.py
@@ -8,10 +8,26 @@ for i in range(len(names)):
     names[i] = random.choice(code_names)
 print(names)
 
+
 # This code is functional
 names = ['Mary', 'Isla', 'Sam']
 
 secret_names = map(lambda x : random.choice(['Mr. Pink', 'Mr. Orange', "Mr. Blonde"]),
                    names)
+
+print(list(secret_names))
+
+# This code is not functional
+names = ['Mary', 'Isla', 'Sam']
+
+for i in range(len(names)):
+    names[i] = hash(names[i])
+
+print(names)
+
+# This code is functional
+names = ['Mary', 'Isla', 'Sam']
+
+secret_names = map(hash, names)
 
 print(list(secret_names))
