acbc8fca6ff5bd4e29f2ff4d3b2d3f9bede822ed
remiconnesson <remiconnesson2@gmail.com>
2022-08-11 09:18:57 +0000

    Demo loop vs. map

---
+++ 01-python-intro/01_randomize_names.py
@@ -0,0 +1,17 @@
+import random
+
+# This code is not functional
+names = ['Mary', 'Isla', 'Sam']
+code_names = ['Mr. Pink', 'Mr. Orange', 'Mr. Blonde']
+
+for i in range(len(names)):
+    names[i] = random.choice(code_names)
+print(names)
+
+# This code is functional
+names = ['Mary', 'Isla', 'Sam']
+
+secret_names = map(lambda x : random.choice(['Mr. Pink', 'Mr. Orange', "Mr. Blonde"]),
+                   names)
+
+print(list(secret_names))
