c34fddd0e8adf31e2e81e2605338dce9461318ce
remiconnesson <remiconnesson2@gmail.com>
2022-08-12 14:17:36 +0000

    Logic Gates with Lambda Calculus <3

--- 02-JS-intro-by-anjana-vakil/04_lambda_calculus.js
+++ 02-JS-intro-by-anjana-vakil/04_lambda_calculus.js
@@ -50,3 +50,42 @@ let coffee = IF_ELSE(tired)(three)(zero)
 
 console.log(lambdaToNumber(coffee))
 
+/*
+ * Logic Gates
+ */
+const lambdaToBoolean = bool => bool(true)(false);
+
+// partial application of the boolean
+const NOT = bool => a => b => bool(b)(a);
+
+const NOT_TRUE = NOT(TRUE)
+console.log(lambdaToBoolean(NOT_TRUE))
+
+const NOT_FALSE = NOT(FALSE)
+console.log(lambdaToBoolean(NOT_FALSE))
+
+// note: A & B sont des booleans lambda (donc sous la forme bool => a => b => bool(b)(a))
+const OR = A => B => A(A)(B)
+
+
+const table = [[FALSE, FALSE], [FALSE, TRUE], [TRUE, FALSE], [TRUE, TRUE]];
+const apply_logic_operation = OPERATOR => OPERANDES => lambdaToBoolean(OPERATOR(OPERANDES[0])(OPERANDES[1]))
+
+console.log("OR")
+// nice use of partial application 
+console.log(table.map(apply_logic_operation(OR)));
+
+const AND = A => B => A(B)(A)
+
+console.log("AND")
+console.log(table.map(apply_logic_operation(AND)))
+
+const NAND = A => B => NOT(AND(A)(B))
+
+console.log("NAND")
+console.log(table.map(apply_logic_operation(NAND)))
+
+const XOR = A => B => AND(OR(A)(B))(NAND(A)(B))
+
+console.log("XOR")
+console.log(table.map(apply_logic_operation(XOR)))
